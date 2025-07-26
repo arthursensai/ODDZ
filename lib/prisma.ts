55bf9e5abdce745366eec5ba83f219ef6ada7e97
arthursensai <aytsidibah@gmail.com>
2025-07-26 14:57:56 +0100

    yaaaaaaaaaa rbe tkhdm

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var __prisma: PrismaClient | undefined
+  var __prisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
 } else {
   if (!global.__prisma) {
-    global.__prisma = new PrismaClient()
+    global.__prisma = new PrismaClient();
   }
-  prisma = global.__prisma
+  prisma = global.__prisma;
 }
 
-export { prisma }
+export { prisma };
